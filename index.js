3cc54ae0551528d7aab9ca243ca94c8b0f12d807
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-06-25 11:16:09 -0400

    started working on the index.js code for inquirer and such

---
+++ index.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle, Square, Triangle} = require('./lib/shape');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3 || 'Text must be up to three characters long',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (color keyword or hexadecimal):',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo:',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (color keyword or hexadecimal):',
+    },
+];
