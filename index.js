ce128936dc13abb4b7236ecddebf995a3d7ff59b
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-06-25 12:26:22 -0400

    created a promp module for inquirer with questions then answers. also created a switchincorporated a switch/case expression to acomodate the users input for shapes. for lastcreated a constant svgLogo with <svg> html code to be writen in a new file( 'logo.svg') along with slectedShape.render() by fs. haven't checked if it works

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle} = require('./lib/shape');
+const shape = require('./lib/shape');
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
 
 const questions = [
     {
@@ -29,4 +32,39 @@ const questions = [
 
 inquirer.createPromptModule(questions).then(answers => {
     const {text, textColor, shape, shapeColor } = answers;
-})
+
+    let selectedShape;
+    switch (shape) {
+        case 'Circle':
+            selectedShape = new Circle();
+            break;
+        case 'Square':
+            selectedShape = new Square();
+            break;
+        case 'Triangle':
+            selectedShape = new Triangle();
+            break;
+        default:
+            throw new Error('Invalid shape type');
+    }
+
+    selectedShape.setColor(shapeColor);
+    selectedShape.setText(text);
+    selectedShape.setTextColor(textColor);
+
+    const svgLogo = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    ${selectedShape.render()}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>
+  `;
+
+  fs.writeFile('logo.svg', svgLogo.trim(), err => {
+    if (err) {
+        console.error('Error writing SVG file:', err);
+        return;
+    }
+    console.log('Generated logo.svg');
+  });
+
+});
